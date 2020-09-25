2aa533aedb7bf6f1a918f4e8797339af4b25920d
Isabella Szemeti <issythegirl@gmail.com>
2020-09-25 01:49:30 +0100

    fix(awd-it): Product parsing

--- awd-it.py
+++ awd-it.py
@@ -2,6 +2,7 @@ import requests, bs4
 from bs4 import BeautifulSoup as soup
 import general
 import re
+import time
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 urls = ['https://www.awd-it.co.uk/components/graphics-cards/nvidia/nvidia-geforce-rtx-3080.html']
@@ -14,20 +15,21 @@ def get_product_data(product):
     d = {}
     d['name'] = product.find('a', {'class': 'product-title'})['title']
     d['url'] = product.find('a', {'class': 'product-title'})['href']
-    d['id'] = product.find('a', {'class': 'product-title'})['id'].split('det_img_')[1]
-    d['image'] = product.find('img', {'class': 'primary'})['data-src']
+    d['id'] = product.find('img', {'class': 'ty-pict'})['id'].strip('det_img_')
+    img = product.find('img', {'class': 'ty-pict'})
+    d['image'] = img['data-src'] if 'data-src' in img.attrs else img['src']
     if product.find('span', {'class': 'ty-qty-out-of-stock'}): # Out of stock
         d['availability'] = 'Out of stock'
     elif product.find('a', {'title': 'Add to Basket'}): # In stock
         d['availability'] = 'In stock'
-    d['price'] = product.find('span', {'class': 'ty-price-num'}).contents[0]
+    d['price'] = product.find('span', {'class': 'ty-price-num'}).nextSibling.contents[0]
     return d
 
 while True:
     for url in urls:
         page = make_soup(url)
         productList = page.find('div', {'class': 'grid-list vs-grid-table-wrapper'}).find_all('div', {'class': 'ty-column4'})
-        productData = [get_product_data(i) for i in products]
+        productData = [get_product_data(i) for i in productList if i.find('img') is not None]
         for product in productData:
             prevState = general.get_prev_state('ariapc', product['id'])
             general.update_db('ariapc', product)
